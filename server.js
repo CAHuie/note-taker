98cd410a60d496739cf9082e4418b2017099987f
chris <christopher.huie3@gmail.com>
2021-11-11 13:03:04 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express=require("express");
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = process.env.PORT||3001;
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// helper functions
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readFromFile = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
